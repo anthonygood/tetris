45f8ee6d0c94ddc9eac844d514f365722cf36cd7
Anthony Good <good.anthony@gmail.com>
2022-09-29 17:58:24 +0100

    rename dist to publish to attract commit

--- publish.sh
+++ publish.sh
@@ -1,7 +1,8 @@
 git checkout build
 rm -rf dist
 npm run build
-git add dist
+cp dist publish
+git add publish
 git commit -m "Publish build to build branch"
 git push origin build
 echo "You're now on the build branch!"
