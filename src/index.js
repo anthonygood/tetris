e75e0fd343361800b9583fd470e5fae2ce80d40b
Anthony Good <good.anthony@gmail.com>
2022-09-29 22:36:40 +0100

    implement clear flash

--- src/index.js
+++ src/index.js
@@ -8,26 +8,49 @@ import { control } from './control'
 
 const TICK_INTERVAL = 250
 const FRAME_INTERVAL = 50
+const CLEAR_INTERVAL = 500
+
+const gameoverEffect = (stop, appContainer) => () => {
+  appContainer.classList.add('gameover')
+  stop()
+}
+
+const clearEffect = (start, stop, update, domCells) => (event) => {
+  const { indices, board } = event
+  update(board.before)
+  stop()
+  indices.forEach(index => {
+    domCells[index].forEach(cell => cell.classList.add('flash'))
+  })
+
+  setTimeout(() => {
+    start()
+    indices.forEach(index => {
+      domCells[index].forEach(cell => cell.classList.remove('flash'))
+    })
+  }, CLEAR_INTERVAL)
+}
 
 const play = () => {
   const tetris = new Tetris()
 
-  const { appContainer, virtualDom } = render(tetris.height(), tetris.width())
+  const {
+    appContainer,
+    domCells
+  } = render(tetris.height(), tetris.width())
 
   tetris.start()
-  update(tetris, virtualDom)
+  const updateBoard = update(tetris, domCells)
 
   const { start, stop } = initStartStop({
     tickFn: () => tetris.tick(),
-    renderFn: () => update(tetris, virtualDom),
+    renderFn: updateBoard,
     tickInterval: TICK_INTERVAL,
     frameInterval: FRAME_INTERVAL,
   })
 
-  tetris.on(Tetris.Events.GAME_OVER, () => {
-    appContainer.classList.add('gameover')
-    stop()
-  })
+  tetris.on(Tetris.Events.GAME_OVER, gameoverEffect(stop, appContainer))
+  tetris.on(Tetris.Events.LINE_CLEAR, clearEffect(start, stop, updateBoard, domCells))
 
   const togglePause = pause(tetris, start, stop)
   document.addEventListener('keydown', control(tetris))
